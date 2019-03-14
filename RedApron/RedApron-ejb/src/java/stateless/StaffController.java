88903d533671555010a4c49b086f2ec5af901848
pereira yip <yippygames@gmail.com>
2019-03-14 22:28:15 +0800

    staff controller added fetching

--- RedApron/RedApron-ejb/src/java/stateless/StaffController.java
+++ RedApron/RedApron-ejb/src/java/stateless/StaffController.java
@@ -5,15 +5,38 @@
  */
 package stateless;
 
+import entity.Staff;
+import exceptions.StaffNotFoundException;
+import javax.ejb.Local;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
  * @author matthealoo
  */
 @Stateless
+@Local(StaffControllerLocal.class)
 public class StaffController implements StaffControllerLocal {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    @PersistenceContext(unitName = "RedApron-ejbPU")
+    private EntityManager em;
+
+    public Staff persist(Staff staff) {
+        em.persist(staff);
+        return staff;
+    }
+
+    public Staff retrieveStaffById(long id) throws StaffNotFoundException{
+        Staff staff = em.find(Staff.class, id);
+
+        if (staff != null) {
+            return staff;
+        } else {
+            throw new StaffNotFoundException("Staff does not exist!");
+        }
+
+    }
+
 }
